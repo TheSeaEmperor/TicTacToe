70302a143c19f52c2a8d369ba6ebb46788bf4dc0
Timothy Robinson <timothyr218@gmail.com>
2018-07-15 21:36:09 -0700

    Added Drag Mechanic to Game Board
    Added ability to drag a Game Piece into slots on the board. Currently only one slot available.

---
+++ Assets/Scripts/DragHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public static GameObject draggableobject;
+    private Vector3 start_pos;
+    private Transform start_parent;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        draggableobject = gameObject;
+        start_pos = transform.position;
+        start_parent = transform.parent;
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+
+        transform.SetParent(transform.root);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //GetComponentInParent<GridLayout>().enabled = false;
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //GetComponentInParent<GridLayout>().enabled = true;
+        draggableobject = null;
+        if (transform.parent == start_parent || transform.parent == transform.root)
+        {
+            transform.position = start_pos;
+            transform.SetParent(start_parent);
+        }
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+}
